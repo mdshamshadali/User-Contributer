1d8def5993d4a97c3eaf4a432a41047767d92bec
Md Shamshad Ali <mdshamshadali134@gmail.com>
2024-09-26 03:39:43 +0530

    user module

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import mongoose from 'mongoose';
+import { User} from './model/user.module';
 
 
 const app = express();
---
+++ backend/model/user.module.js
@@ -0,0 +1,18 @@
+// import mongoose from "mongoose";
+
+// const userShcema = new mongoose.Schema({
+//     name:{
+//         type: String,
+//         require: true
+//     },
+//     email:{
+//         type: String,
+//         require: true
+//     },
+//     phone:{
+//         type: Number,
+//         require: true
+//     }
+// },{timestamps:true})
+
+// export const User = new mongoose.model('User', userShcema);
