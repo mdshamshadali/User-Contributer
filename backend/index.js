13eaa56992332d1218d4b88467892f2a9a56ebec
Md Shamshad Ali <mdshamshadali134@gmail.com>
2024-09-26 03:40:17 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import mongoose from 'mongoose';
-import { User} from './model/user.module';
 
 
 const app = express();
